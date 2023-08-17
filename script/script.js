e00785857a843089106bcab75460fe7ad8a6847c
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-17 18:33:49 +0530

    Add wait component

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 import { Password } from "./commands/password.js";
 import { Bot, User, Setting } from "./utility.js";
 
+import { Wait } from "./components/wait.js";
+
 const form = document.querySelector(".command__form");
 const input = document.querySelector("#command__text");
 const history = document.querySelector(".history");
@@ -17,15 +19,19 @@ form.onsubmit = async (e) => {
     const input_text = input.value;
     su.append_node(input_text);
 
+    const wait_element = Wait.create_filled_element(input_text);
+    ternel.append_node(wait_element);
     const command = ternel.get_command(input_text);
-    const command_data = await command.manage(input_text);
-    const element = command.render_element.create_filled_element(
-        command.name,
-        command.description + ": " + input_text,
-        command_data
-    );
-    console.log(element);
-    ternel.append_node(element);
-
+    if (command) {
+        const command_data = await command.manage(input_text);
+        const element = command.render_element.create_filled_element(
+            command.name,
+            command.description + ": " + input_text,
+            command_data
+        );
+        console.log(element);
+        wait_element.remove();
+        ternel.append_node(element);
+    }
     input.value = "";
 };
