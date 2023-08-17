007295dba8a9a7489672145330c183bf1d2e7472
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-17 19:12:30 +0530

    Implement smooth scroll

--- script/script.js
+++ script/script.js
@@ -20,10 +20,10 @@ form.onsubmit = async (e) => {
     const input_text = input.value;
     su.append_node(input_text);
 
-    const wait_element = Wait.create_filled_element(input_text);
-    const appended_node = ternel.append_node(wait_element);
     const command = ternel.get_command(input_text);
     if (command) {
+        const wait_element = Wait.create_filled_element(input_text);
+        const appended_node = ternel.append_node(wait_element);
         const command_data = await command.manage(input_text);
         const element = command.render_element.create_filled_element(
             command.name,
@@ -32,6 +32,8 @@ form.onsubmit = async (e) => {
         );
         console.log(element);
         ternel.replace_node(appended_node, element);
-    } else ternel.append_node(E404.create_filled_element(input_text));
+        ternel.scroll();
+    } else if (input_text.startsWith(";"))
+        ternel.append_node(E404.create_filled_element(input_text));
     input.value = "";
 };
