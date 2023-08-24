e142e31cc97c06559411ca0f66e0689bb9c85e94
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-24 23:26:49 +0530

    Wiki object literal to class

--- script/commands/wiki.js
+++ script/commands/wiki.js
@@ -1,21 +1,24 @@
 import { Message } from "../components/message.js";
-const Wiki = {
-    name: "wiki",
-    description: "Fetch wikipedia article of a given topic.",
-    component: Message,
+
+class Wiki_class {
+    constructor() {
+        this.name = "wiki";
+        this.description = "Fetch wikipedia article of a given topic.";
+        this.component = Message;
+    }
 
     async execute(topic) {
         topic = this.filter_input_text(topic);
         const data = await this.request_data(topic);
         return ["Wikipedia: " + topic, data];
-    },
+    }
 
     filter_input_text(text) {
         const word_list = text.split(" ");
         if (word_list.length === 1)
             throw new Error("wrong number of arguments");
         return word_list.slice(1).toString();
-    },
+    }
 
     async request_data(topic) {
         const res = await fetch(
@@ -23,12 +26,14 @@ const Wiki = {
         );
         const data = await res.json();
         return data["extract"];
-    },
+    }
 
     help() {
         const help_text = this.description;
         return help_text;
-    },
-};
+    }
+}
+
+const Wiki = new Wiki_class();
 
 export { Wiki };
