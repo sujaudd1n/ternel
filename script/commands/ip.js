9ec5b81bc4dc0f3425ae0313f4b4a04c9d383497
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-25 15:27:20 +0530

    Add ip command

---
+++ script/commands/ip.js
@@ -0,0 +1,51 @@
+import { Lists } from "../components/lists.js";
+
+class IP_class {
+    constructor() {
+        this.name = "ip";
+        this.description = "IP address information.";
+        this.component = Lists;
+    }
+
+    async execute(input_text) {
+        const address = this.filter_input_text(input_text);
+        const data = await this.request_data(address);
+        const filtered_data = this.filter_json_data(data);
+        return [this.name, this.description, filtered_data];
+    }
+
+    filter_input_text(text) {
+        const word_list = text.split(" ");
+        if (word_list.length > 2) throw new Error("Wrong number of arguments.");
+        return word_list[1];
+    }
+
+    async request_data(address) {
+        let url;
+        if (address) url = `https://ipapi.co/${address}/json`;
+        else url = `https://ipapi.co/json`;
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    }
+
+    /**
+     * return array of text
+     *
+     */
+    filter_json_data(json_data) {
+        const data_array = [];
+        for (const key in json_data)
+            data_array.push(`${key} - ${json_data[key]}`);
+        return data_array;
+    }
+
+    help() {
+        const help_text = this.description;
+        return help_text;
+    }
+}
+
+const IP = new IP_class();
+
+export { IP };
