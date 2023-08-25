e382d150857f3286c31f9797cc5b8ada77e82c5d
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-25 15:04:07 +0530

    Add meme

--- script/commands/helper.js
+++ script/commands/helper.js
@@ -3,7 +3,8 @@ import { Copyright } from "./copyright.js";
 import { Settings } from "./settings.js";
 import { Wiki } from "./wiki.js";
 import { Help } from "./help.js";
+import { Meme } from "./meme.js";
 
-const ALL_COMMANDS = [Password, Copyright, Settings, Wiki, Help];
+const ALL_COMMANDS = [Password, Copyright, Settings, Wiki, Help, Meme];
 
 export { ALL_COMMANDS };
