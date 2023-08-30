4f9b40f825c53a56126c5d8d9001a82c56f2e7ba
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-30 23:30:51 +0530

    Add qr

--- script/commands/qr.js
+++ script/commands/qr.js
@@ -7,24 +7,42 @@ class QR_class {
         this.component = Image;
     }
 
-    async execute() {}
+    async execute(input_text) {
+        const query = this.filter_input_data(input_text);
+        const qr_image_url = await this.request_data(query);
+
+        return [qr_image_url];
+    }
 
     /**
      * Fetches qr of text.
      * @param text the text to convert into qr
-     * @returns binary png image data of the text
+     * @returns url of qr image.
      */
-    async request_data(text) {}
+    async request_data(text) {
+        const ENDPOINT = "https://api.qrserver.com/v1/create-qr-code/?";
+        // key -> size, data
+        const url = ENDPOINT + `data=${text}`;
+        const res = await fetch(url);
+        const data = await res.blob();
+        const image_url = URL.createObjectURL(data);
+        return image_url;
+    }
 
     /**
      *
      * @param {string} input_text command text given by user.
      * @returns {string} the text that will be converted into qr
      */
-    filter_input_data(input_text) {}
+    filter_input_data(input_text) {
+        const splitted_text = input_text.split(" ");
+        if (splitted_text.length < 2)
+            throw new Error("Wrong number of arugments.");
+        return splitted_text.slice(1).toString().replaceAll(",", " ");
+    }
 
     /**
-     * 
+     *
      * @returns {string} a help text for user.
      */
     help() {
