c50a0289022a257902bd8e4fc067f7e865d21d94
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-08-31 19:27:42 +0530

    Improve help and refactored some code

--- script/commands/help.js
+++ script/commands/help.js
@@ -8,22 +8,38 @@ import { ALL_COMMANDS } from "./helper.js";
  * - ;help wiki
  * - ;help set ct
  */
-const Help = {
-    name: "help",
-    description: "Help for the user.",
-    component: Lists,
+class Help_class {
+    constructor() {
+        this.name = "help";
+        this.description = "Help for the user.";
+        this.component = Lists;
+    }
 
-    async execute() {
+    async execute(input_text) {
+        const words_list = input_text.split(" ");
+        if (words_list.length === 1) return this.get_all_general_help_text();
+        else {
+            const command_name = words_list[1];
+            for (const command of ALL_COMMANDS) {
+                if (command.name === command_name)
+                    return command.help();
+            }
+        }
+    }
+    get_all_general_help_text() {
         let help_text_list = [];
         for (const command of ALL_COMMANDS) {
-            help_text_list.push(";" + command.name + ' - ' + command.help());
+            help_text_list.push(";" + command.name + " - " + command.help());
         }
         return ["Help", "Usage guide.", help_text_list];
-    },
+    }
 
-    help() {
-        return this.description;
-    },
-};
+    help(sub_command = "") {
+        const description = "Help is the command for user guide on how to use \
+        a paricular command."
+        return ["Help", description, [this.description]];
+    }
+}
 
+const Help = new Help_class();
 export { Help };
