7d27242dd4d8e5208b9a0236c743ee7d35ca493f
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-09-07 22:16:12 +0530

    Refactor

--- script/commands/wiki.js
+++ script/commands/wiki.js
@@ -1,23 +1,28 @@
 import { Message } from "../components/message.js";
+import { Title_lists } from "../components/title_description_lists.js";
 
 class Wiki_class {
     constructor() {
         this.name = "wiki";
         this.description = "Fetch wikipedia article of a given topic.";
         this.component = Message;
+        this.help_component = Title_lists;
     }
 
     async execute(topic) {
         topic = this.filter_input_text(topic);
         const data = await this.request_data(topic);
-        return ["Wikipedia: " + topic, data];
+        return {
+            data: ["Wikipedia: " + topic, data],
+            component: this.component,
+        };
     }
 
     filter_input_text(text) {
         const word_list = text.split(" ");
         if (word_list.length === 1)
             throw new Error("wrong number of arguments");
-        return word_list.slice(1).toString().replaceAll(',', ' ');
+        return word_list.slice(1).toString().replaceAll(",", " ");
     }
 
     async request_data(topic) {
@@ -29,8 +34,10 @@ class Wiki_class {
     }
 
     help() {
-        const help_text = this.description;
-        return help_text;
+        return {
+            data: ["wiki", this.description, [";wiki topic"]],
+            component: this.help_component,
+        };
     }
 }
 
